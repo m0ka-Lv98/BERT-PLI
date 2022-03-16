6a324034ee586286a271a8262d43960b1deb3b81
m0ka_Lv98 <mygw_yukimasa9831@yahoo.co.jp>
2022-03-16 16:52:45 +0900

    modify saving process

--- tools/poolout_tool.py
+++ tools/poolout_tool.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'yshao'
 
-
+import os
 import logging
 import torch
 import json
@@ -40,7 +40,8 @@ def pool_out(parameters, config, gpu_list, _outname):
     model = parameters["model"]
     dataset = parameters["train_dataset"]
     model.eval()
-
+    if os.path.isfile(_outname):
+        os.remove(_outname)
     acc_result = None
     total_loss = 0
     cnt = 0
@@ -54,6 +55,7 @@ def pool_out(parameters, config, gpu_list, _outname):
     result = []
 
     for step, data in enumerate(dataset):
+        
         for key in data.keys():
             if isinstance(data[key], torch.Tensor):
                 if len(gpu_list) > 0:
@@ -62,8 +64,9 @@ def pool_out(parameters, config, gpu_list, _outname):
                     data[key] = Variable(data[key])
 
         results = model(data, config, gpu_list, acc_result, "poolout")
-        results["output"][0].append(data['label'].item())
-        result = result + results["output"]
+        for i, r in enumerate(results["output"]):
+            r.append(data['label'][i].item())
+            result = result + [r]
         cnt += 1
 
         if step % output_time == 0:
@@ -73,17 +76,20 @@ def pool_out(parameters, config, gpu_list, _outname):
                 gen_time_str(delta_t), gen_time_str(delta_t * (total_len - step - 1) / (step + 1))),
                          "%.3lf" % (total_loss / (step + 1)), output_info, '\r', config)
 
-        if save_step > 0 and step % save_step == 0:
-            out_file = open(_outname, 'w', encoding='utf-8')
+        if save_step > 0:# and step % save_step == 0:
+            out_file = open(_outname, 'a', encoding='utf-8')
+            out_line = ''
             for item in result:
                 tmp_dict = {
                     'guid': item[0],
                     'res': item[1],
                     'label': item[2]
                 }
+                #out_line += (json.dumps(tmp_dict, ensure_ascii=False) + '\n')
                 out_line = json.dumps(tmp_dict, ensure_ascii=False) + '\n'
                 out_file.write(out_line)
             out_file.close()
+            result = []
 
     if step == -1:
         logger.error("There is no data given to the model in this epoch, check your data.")
